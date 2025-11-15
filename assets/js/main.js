8c4bda39a9efa3c59fea7f8c45afb9899a27a647
Jose Herrera <joherrer95@gmail.com>
2025-11-15 11:14:35 +1000

    rer

--- assets/js/main.js
+++ assets/js/main.js
@@ -110,8 +110,8 @@
     new Typed('.typed', {
       strings: typed_strings,
       loop: true,
-      typeSpeed: 100,
-      backSpeed: 50,
+      typeSpeed: 40,
+      backSpeed: 30,
       backDelay: 2000
     });
   }
